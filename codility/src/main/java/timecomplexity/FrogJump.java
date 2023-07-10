72c2d34e75ed7faa143d857d132b914772f8468d
Aldo Sinanaj <asinanaj@schubergphilis.com>
2023-07-10 11:17:42 +0200

    FrogJump solution

--- codility/src/main/java/timecomplexity/FrogJump.java
+++ codility/src/main/java/timecomplexity/FrogJump.java
@@ -1,5 +1,13 @@
 package timecomplexity;
 
-public class FrogJmp {
+/**
+ * Count minimal number of jumps from position X to Y.
+ * <a href="https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/">Link</a>
+ */
+public class FrogJump {
 
+    public int solution(int startPosition, int endPosition, int jumpDistance) {
+
+        return (int) Math.ceil((double) (endPosition - startPosition) / jumpDistance);
+    }
 }
--- codility/src/test/java/timecomplexity/FrogJumpTest.java
+++ codility/src/test/java/timecomplexity/FrogJumpTest.java
@@ -1,4 +1,16 @@
-import static org.junit.jupiter.api.Assertions.*;
-class FrogJmpTest {
-  
+package timecomplexity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class FrogJumpTest {
+
+    private final FrogJump sut = new FrogJump();
+
+    @Test
+    void solution() {
+        assertEquals(3, sut.solution(10, 85, 30));
+        assertEquals(0, sut.solution(1, 1, 1));
+    }
 }
